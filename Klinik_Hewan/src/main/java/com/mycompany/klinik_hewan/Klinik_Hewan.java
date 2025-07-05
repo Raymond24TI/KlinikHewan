02f4b1fdee2cefdb41ec00992c3fe3bd46714c16
Raymond24TI <raymond24ti@mahasiswa.pcr.ac.id>
2025-07-05 14:48:06 +0700

    Pembuatan folder awal

---
+++ Klinik_Hewan/src/main/java/com/mycompany/klinik_hewan/Klinik_Hewan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.klinik_hewan;
+
+/**
+ *
+ * @author NNath
+ */
+public class Klinik_Hewan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
